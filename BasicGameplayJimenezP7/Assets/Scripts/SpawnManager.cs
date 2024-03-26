8f19960100733f4cd405d22e5838a24207b30d61
Ethan Jimenez <723068@student.dusd.net>
2024-03-26 14:04:34 -0700

    Im working on challenge 2 atm
    For my game, I have made the balls spawn randomly on the x position

--- BasicGameplayJimenezP7/Assets/Scripts/SpawnManager.cs
+++ BasicGameplayJimenezP7/Assets/Scripts/SpawnManager.cs
@@ -29,10 +29,7 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnRandomAnimal()
     {
-        int animalIndex = Random.Range(0, animalPrefabs.Length);
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, 20), 0, spawnPosZ);
-
-        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+       
     }
 
     void SpawnLeftAnimal()
